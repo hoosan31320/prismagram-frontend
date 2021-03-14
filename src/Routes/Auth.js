475c7127d9ad0eca5e81f48c6fbaa15825d67d99
hoosan31320 <hoos31320@gmail.com>
2021-03-15 01:48:46 +0700

    Auth Route UI part One

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -1,2 +1,24 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+    min-height: 80vh;
+    display: flex;
+    align-items: center;
+    justify-content: center
+`;
+
+const Box = styled.div`
+    ${props => props.theme.whiteBox}
+`;
 // eslint-disable-next-line
-export default () => "Auth";
+export default () => {
+    // eslint-disable-next-line
+    const [action, setAction] = useState("logIn");
+
+    return (
+        <Wrapper>
+            {action === "logIn" ? <Box>Log In</Box> : <Box>Sign Up</Box>}
+        </Wrapper>
+    );
+};
