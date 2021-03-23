75d4397dbabef3d374375b6ec8958537298f94c9
hoosan31320 <hoos31320@gmail.com>
2021-03-23 20:39:02 +0700

    Mini Fix

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -62,7 +62,8 @@ const SearchPresenter = ({ searchTerm, loading, data }) => {
                         <FatText text="No Posts Found" />
                     ) : (
                         data.searchPost.map(post => (
-                            <SquarePost 
+                            <SquarePost
+                                key={post.id}
                                 likeCount={post.likeCount}
                                 commentCount={post.commentCount}
                                 file={post.files[0]}
